b2ffa890b580d821500eec8aaec5d030a7f80a0d
Wandji Collins <collinschuwa@gmail.com>
2024-09-21 23:03:01 +0100

    Working with data types in go

---
+++ datatypes.go
@@ -0,0 +1,60 @@
+package main 
+
+import "fmt"
+
+func main()  {
+	// declare an integer variable x 
+
+	var x int
+
+	// assign the value 10 to the variable x 
+	x = 10
+
+	// print the value of the variable x 
+	fmt.Println(x)
+
+	// declare a floating-point variable y
+
+	var y float64
+
+	// assign the variable 10.5 to the variable y
+
+	y = 10.5
+
+	// print the value of the variable y
+
+	fmt.Println(y)
+
+	// declare a string variable name z
+
+	var z string
+
+	// assign a the value Hello, World! to z
+
+	z = "Hello, World!"
+
+	// print the value of z
+
+	fmt.Println(z)
+
+	//declare a boolean variable
+	var a bool
+
+	// assign the value true to the variable a
+
+	a = true
+
+	// print the value of a
+
+	fmt.Println(a)
+
+
+}
+
+/* the above outputs
+10
+10.5
+Hello, World!
+true
+*/
+
